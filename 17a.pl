5a4bb70b111431f5d7765f051a9eb4e7f1afc9d9
Miroslav Týnovský <miroslav.tynovsky@avast.com>
2020-12-17 20:53:02 +0100

    day 17

---
+++ 17a.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature qw(signatures say);
+no warnings qw(experimental::signatures);
+use Data::Dumper;
+use List::Util qw(sum);
+
+my $active;
+my $z = 0;
+my $x = 0;
+while (<>) {
+    chomp;
+    my @row = split //;
+    my $y = 0;
+    for my $c (@row) {
+        $active->{"$x:$y:$z"} = 1 if $c eq '#';
+        $y++;
+    }
+    $x++
+}
+
+for my $i (1..6) {
+    $active = iteration($active);
+    say join(' ', keys %$active);
+}
+
+say scalar(keys %$active);
+
+sub count_neighbors($active) {
+    my %neighbors;
+    for my $a (keys %$active) {
+        my ($x, $y, $z) = split /:/, $a;
+        for my $i ($x-1 .. $x+1) {
+            for my $j ($y-1 .. $y+1) {
+                for my $k ($z-1 .. $z+1) {
+                    next if $i == $x && $j == $y && $k == $z;
+                    $neighbors{"$i:$j:$k"}++
+                }
+            }
+        }
+    }
+    return \%neighbors
+}
+
+sub iteration($prev_active) {
+    my %active;
+    my $neighbors = count_neighbors($active);
+    for my $cube (keys %$neighbors) {
+        $active{$cube} = 1 if $neighbors->{$cube} == 3;
+        $active{$cube} = 1 if $neighbors->{$cube} == 2 && $prev_active->{$cube};
+    }
+    return \%active;
+}
