613b85a814097988c90e9e8ad14e4a924abfa878
Miroslav Týnovský <miroslav.tynovsky@avast.com>
2020-12-24 00:48:09 +0100

    day 21

---
+++ 21b.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature qw(signatures say);
+no warnings qw(experimental::signatures);
+use Data::Dumper;
+use List::Util qw(sum);
+
+my %all_ingredients;
+my %in_one_of;
+while (my $line = <>) {
+    chomp $line;
+    my ($ingredients, $allergens) = $line =~ /(.*) \(contains (.*)\)/;
+    my @ingredients = split / /, $ingredients;
+    my @allergens = split /, /, $allergens;
+    for my $allergen (@allergens) {
+        $in_one_of{$allergen} = intersect(
+            $in_one_of{$allergen},
+            { map {$_ => 1} @ingredients }
+        );
+    }
+    $all_ingredients{$_}++ for @ingredients;
+}
+
+while (1) {
+    my $hit = 0;
+    for my $allergen (keys %in_one_of) {
+        my @ingredients = keys %{ $in_one_of{$allergen} };
+        my $ingredient = shift @ingredients;
+        if (@ingredients == 0) {
+            for my $other_alergen (keys %in_one_of) {
+                next if $other_alergen eq $allergen;
+                if (exists $in_one_of{$other_alergen}->{$ingredient}) {
+                    delete $in_one_of{$other_alergen}->{$ingredient} ;
+                    $hit = 1
+                }
+            }
+        }
+    }
+    last if ! $hit;
+}
+
+my %clean_ingredients = %all_ingredients;
+for my $potentially_allergic (values %in_one_of) {
+    for my $ingredient (keys %$potentially_allergic) {
+        delete $clean_ingredients{$ingredient}
+    }
+}
+
+say sum values %clean_ingredients;
+
+say join ',', map { (keys %$_)[0] } @in_one_of{sort keys %in_one_of};
+print Dumper \%in_one_of;
+
+
+sub intersect($a, $b) {
+    return {%$b} if ! defined $a;
+    return {%$a} if ! defined $b;
+
+    my %result = %$a;
+    for my $key (keys %result) {
+        delete $result{$key} if ! exists $b->{$key};
+    }
+
+    return \%result
+}
+
+
